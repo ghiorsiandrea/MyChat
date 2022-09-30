3ceec5143fb8fac4f24670f53627f5d207b0e8c2
Mauricio Dalgalarrondo <mdalgalarrondo@switch.com.uy>
2022-09-30 15:42:00 -0300

    The code of the module "Shipping Package" with his respective README.md

---
+++ shipping-package/README.md
@@ -0,0 +1,35 @@
+
+<div align="middle">
+<h1>I will introduce you to "Shipping Package"</h1>
+</div>
+
+
+
+<br>
+<br>
+<div align="middle">
+<img src="src/main/resources/java.png" height="100" width="120"/>
+<img src="src/main/resources/sockets.jpg" height="100" width="120"/>
+</div>
+<br>
+<br>
+
+`In this project we made a library with the common code that we will
+use in two related projects: Client and Server, where we will work
+with Sockets.`
+
+<br>
+
+chat-common was created following "Pildoras Informaticas" 's YouTube Java course.
+<br>
+<div align="middle">
+<img src="src/main/resources/javacourse.png" height="100" width="120"/>
+</div>
+<br>
+
+
+
+
+
+
+<br>
---
+++ shipping-package/src/main/java/org/ghiorsi/ShippingPackage.java
@@ -0,0 +1,47 @@
+package org.ghiorsi;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ShippingPackage implements Serializable {
+
+    /**
+     * This class will be i charge of send the information to the Server, for this, it needs to implement Serializable
+     */
+
+    private String nick, ip, mensaje;
+
+    private ArrayList<String> Ips;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public ArrayList<String> getIps() {
+        return Ips;
+    }
+
+    public void setIps(ArrayList<String> ips) {
+        Ips = ips;
+    }
+}
